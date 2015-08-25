74d256ef2729d30b981f69e3e064dd84830fe50d
Johannes Lumpe <johannes@johanneslumpe.de>
2015-08-25 12:27:52 +0300

    Added catch-all handler

--- example/index.js
+++ example/index.js
@@ -4,13 +4,22 @@ import { Provider } from 'react-redux';
 
 import routes from './routes/';
 import createStore from './createStore';
+import { TRANSITION_TO_ROOT } from './constants/ActionTypes';
 
 const router = createRouter({
   location: HistoryLocation,
   routes
 });
 
-const store = createStore(router)();
+const store = createStore(router, (state, { type }) => {
+  if (type === TRANSITION_TO_ROOT) {
+    // this does not do anything useful right now, but serves
+    // as an example how a catch-all handler could be used
+    const { lastItemId } = state.items;
+    const query = lastItemId ? { lastItemId } : null;
+    return { path: '/', query };
+  }
+})();
 
 router.run((Handler, state) => {
   React.render((
