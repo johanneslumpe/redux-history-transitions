f12bf0df4c94d0c54fb61871f5f88998b44534d2
Johannes Lumpe <johannes@johanneslumpe.de>
2015-08-25 12:15:40 +0300

    Removed separate transition handler file

--- example/createStore.js
+++ example/createStore.js
@@ -1,12 +1,11 @@
 import { createStore, compose } from 'redux';
 import storeEnhancer from 'redux-react-router-transitions';
 
-import transitionHandlers from './transitions/';
 import reducers from './reducers/';
 
 export default (router) => {
   const finalCreateStore = compose(
-    storeEnhancer(transitionHandlers, router),
+    storeEnhancer(router),
     createStore
   );
 
